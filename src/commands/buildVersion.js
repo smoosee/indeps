98caf1977181a27590c6483e68e3347e474d818e
Mostafa Sherif <mosherif87@gmail.com>
2023-01-05 09:49:59 -0500

    add try/catch to writing buildVersion method

--- src/commands/buildVersion.js
+++ src/commands/buildVersion.js
@@ -16,7 +16,11 @@ module.exports = (data) => {
     const versionMapper = map === 'major' ? '$3' : map === 'minor' ? '$2' : '$1';
     let pkgVersion = pkg.version.replace(/^(((.+)\..+)\..+)/g, versionMapper);
     if (increment) {
-        pkgVersion = getNextVersion(increment);
+        try {
+            pkgVersion = getNextVersion(increment);
+        } catch (e) {
+            logger('error', `Could not increment package version`);
+        }
     }
 
     const date = dayjs().format('YYYYMMDD.HHmmss');
@@ -31,10 +35,13 @@ module.exports = (data) => {
         `adding CI build number`,
         `${buildVersion}`);
 
-
-    logger('info', `Writing buildVersion into`, filePath);
-    const cwd = process.cwd();
-    const fileContent = readFileSync(resolve(cwd, filePath), { encoding: 'utf-8' });
-    const updatedContent = fileContent.replace(/\$BUILD_VERSION/, buildVersion);
-    writeFileSync(resolve(cwd, filePath), updatedContent, { encoding: 'utf-8' });
+    try {
+        logger('info', `Writing buildVersion into`, filePath);
+        const cwd = process.cwd();
+        const fileContent = readFileSync(resolve(cwd, filePath), { encoding: 'utf-8' });
+        const updatedContent = fileContent.replace(/\$BUILD_VERSION/, buildVersion);
+        writeFileSync(resolve(cwd, filePath), updatedContent, { encoding: 'utf-8' });
+    } catch (e) {
+        logger('error', 'Could not write buildVersion into', filePath);
+    }
 }
