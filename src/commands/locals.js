199db0627af53544042d26be7e4d67dda3108bc3
Mostafa Sherif <mosherif87@gmail.com>
2023-03-27 11:42:00 -0400

    avoid globally linking already linked libraries

--- src/commands/locals.js
+++ src/commands/locals.js
@@ -1,33 +1,59 @@
-const { execSync } = require('child_process');
-const { lstatSync } = require('fs');
-const { resolve } = require('path');
-const { getConfig, logger, updateJsonFile, getLocalPath } = require('../util');
-
+const { execSync } = require("child_process");
+const { lstatSync } = require("fs");
+const { resolve } = require("path");
+const { getConfig, logger, updateJsonFile, getLocalPath } = require("../util");
 
 module.exports = (data) => {
-    const { dependencies: { locals } } = getConfig();
-    const cwd = process.cwd();
-    const keys = Object.keys(locals||{});
+  const {
+    dependencies: { locals },
+  } = getConfig();
+  const cwd = process.cwd();
+  const keys = Object.keys(locals || {});
 
-    keys.forEach(name => {
+  keys.forEach((name) => {
+    const stats = lstatSync(resolve(cwd, "node_modules", name));
 
-        const stats = lstatSync(resolve(cwd, 'node_modules', name));
-
-        if (stats.isSymbolicLink()) {
-            logger('error', 'Dependency linking for', name, 'already exists in node_modules');
-        } else {
-            const depPath = getLocalPath(name);
-            logger('info', 'Linking dependency', name, `from "${depPath}"`, 'to node_modules');
-
-            const updated = updateJsonFile(depPath, 'package.json', { name });
-
-            if (updated) {
-                logger('info', `Linking ${name}`, `to global node_modules`);
-                execSync(`npm link`, { cwd: resolve(cwd, depPath), stdio: 'inherit' });
-            }
+    if (stats.isSymbolicLink()) {
+      logger(
+        "error",
+        "Dependency linking for",
+        name,
+        "already exists in node_modules"
+      );
+    } else {
+      try {
+        const result = execSync(`npm ls -g ${name}`);
+        if (result) {
+          logger("error", name, "already linked to global node_modules");
         }
-    });
+      } catch (e) {
+        const depPath = getLocalPath(name);
+        logger(
+          "info",
+          "Linking dependency",
+          name,
+          `from "${depPath}"`,
+          "to node_modules"
+        );
 
-    logger('info', `Linking ${keys}`, `from global node_modules`, `to local node_modules`);
-    execSync(`npm link ${keys.join(' ')}`, { cwd, stdio: 'inherit' });
-}
+        const updated = updateJsonFile(depPath, "package.json", { name });
+
+        if (updated) {
+          logger("info", `Linking ${name}`, `to global node_modules`);
+          execSync(`npm link`, {
+            cwd: resolve(cwd, depPath),
+            stdio: "inherit",
+          });
+        }
+      }
+    }
+  });
+
+  logger(
+    "info",
+    `Linking ${keys}`,
+    `from global node_modules`,
+    `to local node_modules`
+  );
+  execSync(`npm link ${keys.join(" ")}`, { cwd, stdio: "inherit" });
+};
