a23e0e1eacc3d63e501b9e47dd1b3b62131515b8
Mostafa Sherif <mosherif87@gmail.com>
2023-03-27 10:50:33 -0400

    feat: allow linking multiple libraries at once

--- src/commands/locals.js
+++ src/commands/locals.js
@@ -6,7 +6,9 @@ const { getConfig, logger, updateJsonFile, getLocalPath } = require('../util');
 
 module.exports = (data) => {
     const { dependencies: { locals } } = getConfig();
-    Object.keys(locals || {}).forEach(name => {
+    const keys = Object.keys(locals||{});
+
+    keys.forEach(name => {
 
         const cwd = process.cwd();
         const stats = lstatSync(resolve(cwd, 'node_modules', name));
@@ -22,9 +24,10 @@ module.exports = (data) => {
             if (updated) {
                 logger('info', `Linking ${name}`, `to global node_modules`);
                 execSync(`npm link`, { cwd: resolve(cwd, depPath), stdio: 'inherit' });
-                logger('info', `Linking ${name}`, `from global node_modules`, `to local node_modules`);
-                execSync(`npm link ${name}`, { cwd, stdio: 'inherit' });
             }
         }
     });
+
+    logger('info', `Linking ${keys}`, `from global node_modules`, `to local node_modules`);
+    execSync(`npm link ${keys.join(' ')}`, { cwd, stdio: 'inherit' });
 }
