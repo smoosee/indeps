407569fefce02284368db92120dd181b07d74159
Mostafa Sherif <mosherif87@gmail.com>
2023-06-02 14:03:52 -0400

    destructure code into methods

--- src/commands/triggerBuild.js
+++ src/commands/triggerBuild.js
@@ -6,43 +6,57 @@ const { getConfig } = require('../util');
 const { library } = getConfig();
 
 async function getCurrentVersion() {
-    const versionFileContent = fs.readFileSync(library.versionFile, 'utf8');
+    const versionFileContent = fs.readFileSync(library.versionFile, "utf8");
     let result;
-    if (library.versionFile.endsWith('.xml')) {
+    if (library.versionFile.endsWith("pom.xml")) {
         result = await xml2js.parseStringPromise(versionFileContent);
     } else if (library.versionFile) {
         result = JSON.parse(versionFileContent);
     }
-    return result?.version || library.version;
+    return []
+        .concat(result?.project?.version, library.version)
+        .filter(Boolean)[0];
 }
 
-const version = getCurrentVersion();
+function createBuildFile(version) {
+    if (!fs.existsSync(library.buildFile)) {
+        fs.writeFileSync(library.buildFile, JSON.stringify({ version, build: 0 }));
+    }
 
-if (!fs.existsSync(library.buildFile)) {
-    fs.writeFileSync(library.buildFile, JSON.stringify({ version, build: 0 }));
+    // Read version and build number from build.json file
+    const buildFileContent = fs.readFileSync(library.buildFile, "utf8");
+    const buildData = JSON.parse(buildFileContent);
+
+    // Pump version and build number
+    const now = new Date();
+    const date = now.toISOString().slice(0, 10);
+    const time = now.toTimeString().slice(0, 8);
+    const sameVersion = buildData.version === version;
+    const sameDate = buildData.date === date;
+    const build = sameVersion && sameDate ? buildData.build + 1 : 1;
+
+    // Write new version, build number, and date to build.json file
+    const newData = { ...buildData, version, build, date, time };
+    fs.writeFileSync(library.buildFile, JSON.stringify(newData, null, 2));
+    return { date, build };
 }
 
-// Read version and build number from build.json file
-const buildFileContent = fs.readFileSync(library.buildFile, 'utf8');
-const buildData = JSON.parse(buildFileContent);
+function commitBuildFile(version, date, build) {
+    const commitMessage = `Trigger build for version ${version} (date ${date}) (build ${build})`;
 
-// Pump version and build number
-const now = new Date();
-const date = now.toISOString().slice(0, 10);
-const time = now.toTimeString().slice(0, 8);
-const build = date === buildData.date ? buildData.build + 1 : 1;
+    // Create git commit for build.json file
+    execSync(`git add ${library.buildFile}`);
+    execSync(`git commit -m "${commitMessage}"`);
 
-// Write new version, build number, and date to build.json file
-const newData = { ...buildData, version, build, date, time };
-fs.writeFileSync(library.buildFile, JSON.stringify(newData, null, 2));
+    // Create git tag for commit
+    execSync(`git tag v${version}-date${date}-build${build}`);
 
-// Create git commit for build.json file
-execSync(`git add ${library.buildFile}`);
-execSync(`git commit -m "Bump version to ${newVersion} (build ${newBuild})"`);
+    // Push commit and tag to remote repo
+    execSync("git push");
+    execSync("git push --tags");
+}
 
-// Create git tag for commit
-execSync(`git tag v${newVersion}-build${newBuild}`);
-
-// Push commit and tag to remote repo
-execSync('git push');
-execSync('git push --tags');
+getCurrentVersion().then((version) => {
+    const { date, build } = createBuildFile(version);
+    commitBuildFile(version, date, build);
+});
